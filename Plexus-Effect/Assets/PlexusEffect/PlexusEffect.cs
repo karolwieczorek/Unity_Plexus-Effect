9b0d2de8fde07d5cea7d8f0a0aad62662303dadf
Karl <karolwieczorek9@o2.pl>
2018-05-24 21:53:12 +0200

    some lights, glow, trails etc

--- Plexus-Effect/Assets/PlexusEffect/PlexusEffect.cs
+++ Plexus-Effect/Assets/PlexusEffect/PlexusEffect.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 namespace Game {
     public class PlexusEffect : MonoBehaviour
     {
-        [SerializeField] float maxDistance = 1.0f;
+        [SerializeField] PlexusEffectData data;
 
         Transform targetTransform;
 
@@ -13,8 +13,10 @@ namespace Game {
 
         ParticleSystem.MainModule particleSystemMainModule;
 
-        [SerializeField]  LineRenderer lineRendererTemplate;
         List<LineRenderer> lineRenderers = new List<LineRenderer>();
+
+        ParticleSystemSimulationSpace simulationSpace;
+        uint connections = 0;
 
         private void Start()
         {
@@ -35,50 +37,81 @@ namespace Game {
             particleSystem.GetParticles(particles);
             int particlesCount = particleSystem.particleCount;
 
-            float maxDistanceSqr = maxDistance * maxDistance;
+            float maxDistanceSqr = data.maxDistance * data.maxDistance;
 
-            ParticleSystemSimulationSpace simulationSpace = particleSystemMainModule.simulationSpace;
+            simulationSpace = particleSystemMainModule.simulationSpace;
             SimulationSpaceAdjust(simulationSpace);
 
             int lrIndex = 0;
-            int lineRendererCount = lineRenderers.Count;
 
             for (int i = 0; i < particlesCount; i++)
             {
-                Vector3 p1_position = particles[i].position;
+
+                if (lrIndex >= data.maxLineRenderers)
+                    break;
+                connections = 0;
+                ParticleSystem.Particle p1 = particles[i];
+
                 for (int j = i + 1; j < particlesCount; j++)
                 {
-                    Vector3 p2_position = particles[j].position;
-                    float distanceSqr = Vector3.SqrMagnitude(p1_position - p2_position);
+                    if (connections >= data.maxConnections || lrIndex >= data.maxLineRenderers)
+                        break;
 
-                    if (distanceSqr <= maxDistanceSqr)
-                    {
-                        LineRenderer lr;
-
-                        if (lrIndex == lineRendererCount)
-                        {
-                            lr = Instantiate(lineRendererTemplate, targetTransform, false);
-                            lineRenderers.Add(lr);
-
-                            lineRendererCount++;
-                        }
-
-                        lr = lineRenderers[lrIndex];
-
-                        lr.enabled = true;
-                        lr.useWorldSpace = simulationSpace == ParticleSystemSimulationSpace.World;
-                        lr.SetPosition(0, p1_position);
-                        lr.SetPosition(1, p2_position);
-
-                        lrIndex++;
-                    }
+                    ParticleSystem.Particle p2 = particles[j];
+                    lrIndex = SetupLineRenderer(maxDistanceSqr, lrIndex, p1, p2);
                 }
             }
 
-            for (int i = lrIndex; i < lineRendererCount; i++)
+            for (int i = lrIndex; i < lineRenderers.Count; i++)
             {
                 lineRenderers[i].enabled = false;
             }
+        }
+
+        private int SetupLineRenderer(float maxDistanceSqr, int lrIndex, ParticleSystem.Particle p1, ParticleSystem.Particle p2)
+        {
+            float distanceSqr = Vector3.SqrMagnitude(p1.position - p2.position);
+            int lineRendererCount = lineRenderers.Count;
+
+            if (distanceSqr <= maxDistanceSqr)
+            {
+                LineRenderer lr;
+
+                if (lrIndex == lineRendererCount)
+                {
+                    lr = Instantiate(data.lineRendererTemplate, targetTransform, false);
+                    lineRenderers.Add(lr);
+
+                    lineRendererCount++;
+                }
+
+                lr = lineRenderers[lrIndex];
+
+                lr.enabled = true;
+                lr.material = data.lineRendererMaterial;
+                lr.startWidth = lr.endWidth = data.width;
+                lr.useWorldSpace = simulationSpace == ParticleSystemSimulationSpace.World;
+                lr.SetPosition(0, p1.position);
+                lr.SetPosition(1, p2.position);
+                lr.colorGradient = ParticleGradientColor(p1, p2);
+
+                lrIndex++;
+                connections++;
+            }
+            return lrIndex;
+        }
+
+        private Gradient ParticleGradientColor(ParticleSystem.Particle p1, ParticleSystem.Particle p2)
+        {
+            Gradient gradient = new Gradient();
+            float alpha = 1f;
+            Color32 p1_color = p1.GetCurrentColor(particleSystem);
+            Color32 p2_color = p2.GetCurrentColor(particleSystem);
+            gradient.SetKeys(
+                new GradientColorKey[] { new GradientColorKey(p1_color, 0.0f), new GradientColorKey(p2_color, 1.0f) },
+                new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
+                );
+            return gradient;
         }
 
         private void SimulationSpaceAdjust(ParticleSystemSimulationSpace simulationSpace)
@@ -88,19 +121,16 @@ namespace Game {
                 case ParticleSystemSimulationSpace.Local:
                     {
                         targetTransform = transform;
-                        lineRendererTemplate.useWorldSpace = false;
                         break;
                     }
                 case ParticleSystemSimulationSpace.Custom:
                     {
                         targetTransform = particleSystemMainModule.customSimulationSpace;
-                        lineRendererTemplate.useWorldSpace = false;
                         break;
                     }
                 case ParticleSystemSimulationSpace.World:
                     {
                         targetTransform = transform;
-                        lineRendererTemplate.useWorldSpace = true;
                         break;
                     }
                 default:
---
+++ Plexus-Effect/Assets/PlexusEffect/PlexusEffectData.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Game
+{    
+    [CreateAssetMenu(fileName = "PlexusEffectData", menuName = "Custom/PlexusEffectData", order = 1)]
+    public class PlexusEffectData : ScriptableObject
+    {
+        public float maxDistance = 1.0f;
+        public float width = 0.1f;
+        public uint maxConnections = 5;
+        public uint maxLineRenderers = 100;
+        public LineRenderer lineRendererTemplate;
+        public Material lineRendererMaterial;
+    }
+}
